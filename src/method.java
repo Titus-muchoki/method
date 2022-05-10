175244b4b6ce7f1a22da823ce334050527357023
kajela <titoyut56@gmail.com>
2022-05-10 06:51:02 -0400

    Initial commit

---
+++ src/method.java
@@ -0,0 +1,12 @@
+import org.w3c.dom.css.CSSStyleRule;
+
+public class method {
+    static void myMethod() {
+        System.out.println("i want to do my best in the next 90 days");
+    }
+    public static void main(String[] args) {
+
+        myMethod();
+    }
+
+}
